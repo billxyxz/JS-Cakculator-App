04dc17737fddbab5664eb08dbb38c00c31459050
Collins Erobaga <erobagacollins@gmail.com>
2023-06-14 03:15:25 +0100

    modified the calc.js

--- calc.js
+++ calc.js
@@ -7,24 +7,51 @@ buttons.forEach(button => {
         switch(e.target.innerText){
             case "AC":
                 curr.innerText = ""
+                prev.innerText = ""
                 break;
             case "DEL":
                 if(curr.innerText){
                     curr.innerText = curr.innerText.slice(0, -1)
+                }else if(prev.innerText){
+                    prev.innerText = prev.innerText.slice(0, -1)
                 }
                 break;
+            case "+":
+                prev.innerText += curr.innerText + "+";
+                curr.innerText = ""
+                break;
+            case "%":
+                    prev.innerText += curr.innerText + "%";
+                    curr.innerText = ""
+                    break;
+            case "÷":
+                    prev.innerText += curr.innerText + "÷";
+                    curr.innerText = ""
+                    break;  
+            case "×":
+                    prev.innerText += curr.innerText + "×";
+                    curr.innerText = ""
+                    break;
+            case "-":
+                prev.innerText += curr.innerText + "-";
+                curr.innerText = ""
+                break;    
             case "=":
-                if(curr.innerText.includes("÷")){
-                    const value = curr.innerText.replace("÷", "/")
+                if(prev.innerText.concat(curr.innerText).includes("÷")){
+                    const value = prev.innerText.concat(curr.innerText).replace("÷", "/")
                     curr.innerText = eval(value);
-                }else if(curr.innerText.includes("×")){
-                    const value = curr.innerText.replace("×", "*")
+                    prev.innerText = ""
+                }else if(prev.innerText.concat(curr.innerText).includes("×")){
+                    const value = prev.innerText.concat(curr.innerText).replace("×", "*")
                     curr.innerText = eval(value);
-                }else if(curr.innerText.includes("%")){
-                    const value = curr.innerText.replace("%", "") + "/100"
-                    curr.innerText = eval(value)
+                    prev.innerText = ""
+                }else if(prev.innerText.concat(curr.innerText).includes("%")){
+                    const value = prev.innerText.concat(curr.innerText).replace("%", "") + "/100"
+                    curr.innerText = eval(value);
+                    prev.innerText = ""
                 }
-                curr.innerText = eval(curr.innerText);
+                curr.innerText = eval(prev.innerText.concat(curr.innerText));
+                prev.innerText = ""
                 break;
                 default:
                     curr.innerText += e.target.innerText
